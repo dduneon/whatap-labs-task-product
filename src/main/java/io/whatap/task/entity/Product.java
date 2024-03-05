3de2413ca9db3ec0a08f5ceccb672d80cfe2da43
dduneon <dduneon@gmail.com>
2024-03-05 22:33:14 +0900

    FEAT: Product 생성자 추가

--- src/main/java/io/whatap/task/entity/Product.java
+++ src/main/java/io/whatap/task/entity/Product.java
@@ -3,7 +3,11 @@ package io.whatap.task.entity;
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 /**
  * 상품에 대한 정보를 담고 있는 Product 엔티티 클래스
@@ -13,6 +17,9 @@ import lombok.Getter;
  */
 
 @Getter
+@AllArgsConstructor
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 public class Product extends PanacheEntity {
     // id 필드는 PanacheEntity 에서 상속되었기 때문에 별도로 선언할 필요가 없음
