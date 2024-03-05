cf1a0e98d0d3b448b8e81e1b1a08dc32734af6c4
dduneon <dduneon@gmail.com>
2024-03-06 02:40:49 +0900

    FEAT: Request Dto Validation 설정

--- src/main/java/io/whatap/task/dto/req/ProductUpdateRequestDto.java
+++ src/main/java/io/whatap/task/dto/req/ProductUpdateRequestDto.java
@@ -1,19 +1,21 @@
 package io.whatap.task.dto.req;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 /**
- * 상품 생성을 위한 Request Dto 클래스
+ * description
  *
  * @author 김준현
- * @version 2024. 03. 05
+ * @version 2024. 03. 06
  */
-
 @NoArgsConstructor
 @Getter
-public class ProductCreateRequestDto {
+public class ProductUpdateRequestDto {
+    @NotNull(message = "ID 가 비어 있습니다")
+    private Long id;
     @NotBlank(message = "Name 은 반드시 입력되어야 합니다")
     private String name;
     @NotBlank(message = "Description 은 반드시 입력되어야 합니다")
