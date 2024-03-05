3fee1875dac15410170bf675303a8549e9f27e61
dduneon <dduneon@gmail.com>
2024-03-06 02:40:22 +0900

    FEAT: Update, Delete, Pagination 메서드 작성

--- src/main/java/io/whatap/task/controller/ProductController.java
+++ src/main/java/io/whatap/task/controller/ProductController.java
@@ -1,19 +1,24 @@
 package io.whatap.task.controller;
 
+import io.quarkus.panache.common.Sort;
 import io.whatap.task.dto.req.ProductCreateRequestDto;
-import io.whatap.task.dto.res.ProductResponseDto;
+import io.whatap.task.dto.req.ProductUpdateRequestDto;
 import io.whatap.task.entity.Product;
 import io.whatap.task.service.ProductService;
 import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
 import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
-import lombok.RequiredArgsConstructor;
+import java.util.Objects;
 
 /**
  * 상품 API 를 제공하는 Product REST Controller
@@ -23,36 +28,50 @@ import lombok.RequiredArgsConstructor;
  */
 
 @Path("/products")
-@RequiredArgsConstructor
 public class ProductController {
     private final ProductService productService;
 
-    // Annotation 해도 되고 안해도 됨
-//    @Inject
-//    public ProductController(ProductService productService) {
-//        this.productService = productService;
-//    }
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
 
     @GET
     @Path("/{productId}")
     @Produces(MediaType.APPLICATION_JSON)
     public Response getProduct(@PathParam("productId") Long productId) {
-        ProductResponseDto productResponseDto = productService.readProduct(productId);
-        return Response.ok(productResponseDto)
-                .type(MediaType.APPLICATION_JSON)
-                .build();
+        Product product = Product.findById(productId);
+        if(Objects.isNull(product))
+            return Response.status(Response.Status.NOT_FOUND).build();
+        return Response.ok(product).build();
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getProductsByPagination(@QueryParam("page") int page, @QueryParam("size") int size) {
+        return Response.ok(Product.findAll(Sort.by("id")).page(page, size).list()).build();
     }
 
     @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response createProduct(@Valid ProductCreateRequestDto productCreateRequestDto) {
+        productService.createProduct(productCreateRequestDto);
+        return Response.status(Response.Status.CREATED).build();
+    }
+
+    @PUT
     @Transactional
     @Consumes(MediaType.APPLICATION_JSON)
-    public Response createProduct(ProductCreateRequestDto productCreateRequestDto) {
-//        productService.createProduct(productCreateRequestDto);
-        Product product = Product.builder()
-                .name(productCreateRequestDto.getName())
-                .description(productCreateRequestDto.getDescription())
-                .build();
-        product.persist();
-        return Response.status(Response.Status.CREATED).build();
+    public Response updateProduct(@Valid ProductUpdateRequestDto productUpdateRequestDto) {
+        Product product = Product.findById(productUpdateRequestDto.getId());
+        product.updateProductName(productUpdateRequestDto.getName());
+        product.updateProductDescription(productUpdateRequestDto.getDescription());
+        return Response.ok().build();
+    }
+
+    @DELETE
+    @Transactional
+    @Path("/{productId}")
+    public Response deleteProduct(@PathParam("productId") Long productId) {
+        return Response.ok(Product.deleteById(productId)).build();
     }
 }
